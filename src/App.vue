e457eaad88b30084196fa87bd4e952e59b37d178
kefeng <kefeng1026@gmail.com>
2022-07-30 21:27:24 +0800

    feat: loading page

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,97 @@
 <template>
-	<div class="main-layout">
-		<div class="loading" v-show="isLoading">loading...</div>
-		<div v-show="!isLoading">
-			<Back v-show="!hideBackBtn" />
-			<router-view />
-		</div>
-	</div>
+  <div class="main-layout">
+    <Loading
+      :progress="progress"
+      v-if="isLoading"
+      @loaded="isLoading = false"
+    ></Loading>
+    <div v-show="!isLoading">
+      <Back v-show="!hideBackBtn" />
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Back from 'comps/Back.vue';
-import { useRoute } from 'vue-router';
-import { watch, ref, onMounted } from 'vue';
+import Back from 'comps/Back.vue'
+import Loading from 'comps/Loading.vue'
+import { useRoute } from 'vue-router'
+import { watch, ref, onMounted } from 'vue'
 
 // ts-ignore
-import { imgsPreloader } from '@/utils/imgPreloader.js';
+import { imgsPreloader } from '@/utils/imgPreloader.js'
 // ts-ignore
-import imgPreloaderList from '@/utils/imgPreloaderList.js';
+import imgPreloaderList from '@/utils/imgPreloaderList.js'
 
-const isLoading = ref(true);
+const isLoading = ref(true)
+let progressInterval: any = null
+const progress = ref(0)
+
 onMounted(() => {
-	const load = () => {
-		imgsPreloader(imgPreloaderList).then(() => {
-			isLoading.value = false;
-			console.log('======');
-		}).catch((e: any) => {
-			console.log('======', e);
-			isLoading.value = false;
-		});
-	};
+  // mock load
+  const fakeLoad = () => {
+    if (progressInterval) clearInterval(progressInterval)
 
-	load();
-});
+    progressInterval = setInterval(() => {
+      progress.value += 2
+      if (progress.value >= 100) {
+        clearInterval(progressInterval)
+      }
+    }, 100)
+  }
 
-const HideBackRoutes = ['/'];
-const route = useRoute();
-const hideBackBtn = ref(false);
+  const load = () => {
+    imgsPreloader(imgPreloaderList)
+      .then(() => {
+        isLoading.value = false
+        console.log('======')
+      })
+      .catch((e: any) => {
+        console.log('======', e)
+        isLoading.value = false
+      })
+  }
+
+  // load()
+
+  fakeLoad()
+})
+
+const HideBackRoutes = ['/']
+const route = useRoute()
+const hideBackBtn = ref(false)
 watch(
-	() => route.path,
-	(newPath) => {
-		console.log('=====', newPath);
-		hideBackBtn.value = HideBackRoutes.some((item) => item === newPath);
-	},
-	{
-		immediate: true,
-	}
-);
+  () => route.path,
+  (newPath) => {
+    console.log('=====', newPath)
+    hideBackBtn.value = HideBackRoutes.some((item) => item === newPath)
+  },
+  {
+    immediate: true,
+  }
+)
 </script>
 
 <style lang="scss">
 #app {
-	height: 100%;
-	width: 100%;
+  height: 100%;
+  width: 100%;
 }
 .loading {
-	width: 100%;
-	height: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .main-layout {
-	width: 100%;
-	height: 100%;
-	> section {
-		width: 100%;
-		height: 100%;
-	}
+  width: 100%;
+  height: 100%;
+  > section {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="loading-wrap">
+    <img
+      v-if="showLogo"
+      class="single-logo"
+      src="../assets/single-logo.png"
+      alt="logo"
+    />
+
+    <div class="intro-wrap" v-if="showIntro">
+      <div class="intro-text">你好～</div>
+      <div class="intro-text delay">这里是...</div>
+    </div>
+
+    <img
+      v-if="showLogoWithTitle"
+      class="logo-with-title"
+      src="../assets/logo.png"
+      alt="logo"
+    />
+
+    <div v-if="showProgress" class="loading-progress">
+      <span class="number start">00</span>
+      <div class="progress">
+        <div class="loaded" :style="`width: ${(progress / 100) * 200}px`"></div>
+      </div>
+      <span class="number end">{{ currentProgress || '00' }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, toRef, watch } from 'vue'
+
+const showLogo = ref(true)
+const showIntro = ref(false)
+const showProgress = ref(true)
+const showLogoWithTitle = ref(false)
+
+const props = defineProps({
+  progress: {
+    type: Number,
+    default: 0,
+  },
+})
+const emit = defineEmits(['loaded'])
+const currentProgress = toRef(props, 'progress')
+
+watch(
+  () => currentProgress.value,
+  (newProgress) => {
+    console.log(newProgress)
+
+    if (currentProgress.value > 99) {
+      showIntro.value = true
+      showLogo.value = false
+      showProgress.value = false
+
+      setTimeout(() => {
+        showIntro.value = false
+        showLogoWithTitle.value = true
+
+        setTimeout(() => {
+          emit('loaded')
+        }, 2000)
+      }, 2500)
+    }
+  }
+)
+</script>
+
+<style lang="scss" scoped>
+@mixin fixed-center {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+.loading-wrap {
+  @include fixed-center();
+  background: #090b0d;
+  z-index: 99;
+
+  .single-logo {
+    width: 96px;
+    height: 96px;
+    @include fixed-center();
+  }
+
+  .loading-progress {
+    position: absolute;
+    bottom: 160px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+
+    .progress {
+      width: 200px;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.32);
+      position: relative;
+      margin: 0 10px;
+      overflow: hidden;
+
+      .loaded {
+        position: absolute;
+        left: 0;
+        height: 1px;
+        background: #c4c4c4;
+        transition: width 0.3s;
+      }
+    }
+
+    .number {
+      font-size: 14px;
+      display: inline-block;
+      width: 26px;
+      text-align: right;
+    }
+  }
+
+  @keyframes fadeIn {
+    from {
+      top: 20px;
+      opacity: 0;
+    }
+    to {
+      top: 0;
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeInOut {
+    0% {
+      top: 20px;
+      opacity: 0;
+    }
+    50% {
+      top: 0;
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  .intro-wrap {
+    @include fixed-center();
+    font-size: 48px;
+    animation: fadeIn 1s;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .intro-text {
+      position: relative;
+
+      &.delay {
+        animation: fadeIn 1s 1.5s forwards;
+        opacity: 0;
+      }
+    }
+  }
+
+  .logo-with-title {
+    @include fixed-center();
+    animation: fadeInOut 2s;
+  }
+}
+</style>
